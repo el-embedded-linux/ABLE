63c97a170211ba5861f42dac374199a2dcd1c3f1
Moerai <friendnt@naver.com>
2018-08-10 03:27:55 +0900

    TFT LCD모니터와 ReedSW연결, 자석으로 실험아직 못해봄

--- IoT/nano/nano VR/nanovr_new/nanovr_new.ino
+++ IoT/nano/nano VR/nanovr_new/nanovr_new.ino
@@ -1,5 +1,7 @@
 #include <Time.h>
 #include <TimeLib.h>
+#include <LiquidCrystal.h>
+#include <Adafruit_NeoPixel.h>
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9340.h"
@@ -9,6 +11,7 @@
     #define F(string_literal) string_literal
 #endif
 
+//TFT LCD모니터 연결
 #define _sclk 13
 #define _miso 12
 #define _mosi 11
@@ -16,11 +19,32 @@
 #define _dc 9
 #define _rst 8
 
+
 Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _rst);
 
-int distance=12;
-int spd = 13;
 String today;
+
+float radius = 20; // 바퀴당 이동 거리를 확인 하기 위해 자전거 바퀴의 반지름을 입력해 줍니다.(Cm 단위)
+float circle = (2 * radius * 3.14) / 100;  // 자전거 바퀴의 둘레를 계산(단위를 m로 바꿔주기 위해 100을 나눕니다.)
+
+float bySpeed = 0; // 자전거의 속도
+float ckTime = 0;  // 리드스위치가 
+float uckTime = 0; // Unckecked
+float cycleTime = 0;  // 리드스위치가 인식이 안됬을 시간 부터 인식됬을 때까지의 시간
+float distance = 0; // 자전거의 누적 이동 거리
+float lcdDis = 0; // 자전거의 이동 거리를 LCD출력에 맞게 바꿔즌 값.(단위 수정 or 소숫점 제거)
+
+int count = 0;  // 리드스위치의 노이즈를 제거하기 위해 카운트를 넣어줍니다.
+boolean temp = 0;  // 리드 스위치가 닫혔는지 확인하는 변수
+
+// LED 개수, 아두이노 디지털 핀, 네오픽셀 컬러 타입 + 네오픽셀 클럭
+Adafruit_NeoPixel matrix = Adafruit_NeoPixel(NUMPIXELS, ADAMATRIX, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel bar = Adafruit_NeoPixel(BARPIXELS, BARWING, NEO_GRB + NEO_KHZ800);
+
+int btLeft, btRight, btLCD;  // 좌우 버튼
+int c = 0;            // 버튼 동시 제어 변수
+int presRead;         // 압력센서
+int presPoint;
 
 void setup() {
   Serial.begin(9600);
@@ -42,10 +66,34 @@ void setup() {
 
 void loop(void) {
   //걍 테스트용으로 번갈아가면서 출력하게 했음
-    distanceText();
-    delay(1000);
-    speedText();
-    delay(1000);
+  boolean check = digitalRead(A1); // 리드스위치의 상태를 확인합니다.(SIG=A0)
+  Serial.print(check);
+  if(check == 1 && temp == 0){  // 리드 스위치가 열릴 때(닫힘 -> 열림)
+    ckTime = millis();  // 시간을 확인해서 저장합니다.
+    temp = 1;  // temp값을 1로 바꿔줍니다.(리드스위치가 열려있는 상태값 저장)
+  }
+  
+  else if(check == 0 && temp == 1 && count > 5){  // 리드 스위치가 닫히고(열림 -> 닫힘), 노이즈 방지 카운트가 5이상일때
+    uckTime = millis();  // 시간을 확인해서 저장합니다.
+      
+    cycleTime = (uckTime - ckTime) / 1000;
+    // 열릴 때 시각과 닫힐 때 시각의 차를 이용하여 바퀴가 한바퀴 돌때 걸린 시간을 계산합니다.
+    bySpeed = (circle / cycleTime) * 3.6; // 바퀴가 한바퀴 돌때의 거리와 시간을 가지고 속도를 구해줍니다.(단위는 Km/h입니다.)
+    temp = 0; 
+    count = 0; 
+    distance += circle;  // 한바퀴 돌았으면 이동거리를 누적 이동거리에 더해줍니다.
+  }
+  
+  if(check == 1){  // 리드 스위치가 열려있으면 카운트를 1씩 증가 시켜 줍니다.
+    count++;
+    if(count > 150){ // 카운트가 150이 넘어가면(자전거가 멈췄을 때) 속도를 0으로 바꿔줍니다.
+      bySpeed = 0;
+    }
+  }
+  distanceText();
+  delay(1000);
+  speedText();
+  delay(1000);
 }
 
 void date(){
@@ -67,7 +115,7 @@ unsigned long distanceText() {
   tft.fillScreen(ILI9340_BLACK);
   unsigned long start = micros();
   
-  tft.setCursor(65, 40);
+  tft.setCursor(85, 40);
   tft.setTextColor(ILI9340_WHITE);    tft.setTextSize(3);
   tft.println(today);
 
@@ -77,10 +125,10 @@ unsigned long distanceText() {
   tft.println("");
 
   if(distance <10){
-    tft.setCursor(70, 120);
+    tft.setCursor(20, 120);
   }
   else if (distance <100){
-    tft.setCursor(60, 120);
+    tft.setCursor(10, 120);
   }
   else if (distance <1000){
     tft.setCursor(40, 120);
@@ -99,7 +147,7 @@ unsigned long speedText() {
   tft.fillScreen(ILI9340_BLACK);
   unsigned long start = micros();
   
-  tft.setCursor(65, 40);
+  tft.setCursor(85, 40);
   tft.setTextColor(ILI9340_WHITE);    tft.setTextSize(3);
   tft.println(today);
 
@@ -110,15 +158,15 @@ unsigned long speedText() {
 
 
   //속도 출력
-  if(spd <10){
-    tft.setCursor(120, 110);
+  if(bySpeed <10){
+    tft.setCursor(50, 110);
   }
-  else if (spd <100){
-    tft.setCursor(110, 110);
+  else if (bySpeed <100){
+    tft.setCursor(60, 110);
   }
 
    tft.setTextColor(ILI9340_WHITE);    tft.setTextSize(8);
-   tft.println(spd);
+   tft.println(bySpeed);
 
    tft.setCursor(120, 180);
    tft.setTextSize(3);
